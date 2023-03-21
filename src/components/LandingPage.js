98cb06fcf89832b5c868c0720b1a658c471a6dea
Pisen99 <patricia.andersson99@hotmail.com>
2023-03-21 08:18:32 +0000

    Feat: Styled LandingPage h1. Imported useState

--- src/components/LandingPage.js
+++ src/components/LandingPage.js
@@ -1,18 +1,25 @@
-import React from 'react';
+import React, { useState } from "react";
 import { Container } from 'react-bootstrap';
 import "../styles/LandingPage.module.css";
 import Button from 'react-bootstrap/Button';
-import ButtonGroup from 'react-bootstrap/ButtonGroup';
 
 
 const LandingPage = () => {
+    // Stores the items.
+    // eslint-disable-next-line
+    const [list, setList] = useState([]);
+    // Keeps track on what the item value is.
+    const [input, setInput] = useState("");
+
     return (
-        <Container>
+        <Container fluid="md">
             <h1>Try it out here <i class="fa-solid fa-arrow-down"></i></h1>
-            <ButtonGroup aria-label="Basic example">
-                <Button variant="secondary">Add</Button>
-                <Button variant="secondary">Delete</Button>
-            </ButtonGroup>
+            <input
+                type="text"
+                value={input}
+                onChange={(e) => setInput(e.target.value)}
+            />
+            <Button>Add</Button>
             <p>To get full access please login or signup</p>
         </Container>
     )
